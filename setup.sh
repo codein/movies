4bbb25c6c90278500bb5b615f55c7a9bd844ae7b
codein <robin.codein@gmail.com>
2014-08-25 02:11:17 -0400

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+sudo apt-get install git
+sudo apt-get install python-pip
+sudo apt-get install libmysqlclient-dev python-dev
+sudo apt-get install tmux
+sudo apt-get install nginx
+
+#setup elastic serach
+cd ~
+sudo apt-get update
+sudo apt-get install openjdk-7-jre-headless -y
+### Check http://www.elasticsearch.org/download/ for latest version of ElasticSearch and replace wget link below
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.2.deb
+sudo dpkg -i elasticsearch-1.3.2.deb
+sudo service elasticsearch start
+curl http://localhost:9200
+
+sudo pip install -r requirements.txt
+
+# setup db
+python data/sql_orm.py
+# load data
+cd data
+python data_engineering.py
+
+# run server
+./run_server
+
+# unitest
+./test_runner.sh
+
